45c5590b5b7dde102acc7faea7719b9aedfb7e23
kishino <kisino0325@gmail.com>
2025-05-30 15:01:47 +0900

    cs0530

--- cs0530/Form1.cs
+++ cs0530/Form1.cs
@@ -2,9 +2,30 @@ namespace cs0530
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left < 0)
+            {
+                vx = -vx;
+            }
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
